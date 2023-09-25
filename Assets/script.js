e18d260b6f8fff2bff32c6a20ddd83c0d3c19a79
DillenGrant <dillen.grant72@gmail.com>
2023-09-25 12:19:43 -0600

    click

--- Assets/script.js
+++ Assets/script.js
@@ -2,6 +2,11 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  $(".saveBtn").on("click", function() {
+    var userInput = $(this).siblings("textarea").val();
+    var timeBlockID = $(this).closest(".time-block").attr("id");
+    localStorage.setItem(timeBlockID, userInput);
+});
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
